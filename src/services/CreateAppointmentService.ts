7a53b9c49ed11e0cfae8e9041ce137bf1d5e395d
Higor Matheus O Vieira <hmoliveira83@gmail.com>
2020-11-05 09:41:57 -0300

    add services

---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,37 @@
+import { startOfHour } from 'date-fns';
+import Appointment from '../models/appointment';
+import AppointmentsRepository from '../repositories/appointmentsRepository';
+
+interface Request {
+  provider: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  private appointmentRepository: AppointmentsRepository;
+
+  constructor(appointmentsRepository: AppointmentsRepository) {
+    this.appointmentRepository = appointmentsRepository;
+  }
+
+  public execute({ provider, date }: Request): Appointment {
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = this.appointmentRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw Error('this apppointment is alreeady booked');
+    }
+
+    const appointment = this.appointmentRepository.create({
+      provider,
+      date: appointmentDate,
+    });
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
