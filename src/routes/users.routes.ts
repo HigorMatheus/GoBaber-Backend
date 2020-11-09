25d6e2786377a567bddd9fd22266afaf392cac16
Higor Matheus O Vieira <hmoliveira83@gmail.com>
2020-11-09 09:23:27 -0300

    add uploud de imagens

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploudComfig from '../config/upload';
 import UserMap from '../Mappers/User';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import CreateUserService from '../services/CreateUsersService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 const usersRouter = Router();
+const upload = multer(uploudComfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -15,7 +20,7 @@ usersRouter.post('/', async (request, response) => {
       password,
     });
 
-    const user = UserMap.toDTO(userdate);
+    const user = await UserMap.toDTO(userdate);
 
     return response.json(user);
   } catch (err) {
@@ -23,4 +28,25 @@ usersRouter.post('/', async (request, response) => {
   }
 });
 
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const userdate = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      const user = UserMap.toDTO(userdate);
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
+  },
+);
 export default usersRouter;
